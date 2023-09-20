093f6d0a650a90ec0537685fbe9287381d7fb155
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-20 17:27:09 -0300

    feat: implements assertion testing

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+/* eslint-disable no-console */
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        // eslint-disable-next-line no-throw-literal
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch {
+    console.error(`${titulo} não passou!!!`);
+  }
+};
+
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
