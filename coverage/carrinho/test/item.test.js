815f2f7c3aa7ad923dc328e300a1dfbbdc1c56f6
KatiaSonally <katiasonally@yahoo.com.br>
2023-09-22 18:51:59 -0300

    feat: implements unit tests in jest

---
+++ coverage/carrinho/test/item.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import Item from '../item';
+
+describe('Teste dos itens', () => {
+  it('Deve ter 3 campos: nome, valor e quantidade', () => {
+    const item = new Item('Beterraba', 2.5, 10);
+
+    expect(item.nome).toBe('Beterraba');
+    expect(item.valor).toBe(2.5);
+    expect(item.quantidade).toBe(10);
+  });
+
+  it('Deve ter o preço calculado de acordo com a quantidade', () => {
+    const item = new Item('Batata', 0.3, 10);
+
+    expect(item.pegaValorTotalItem()).tobe(3);
+  })
+});
